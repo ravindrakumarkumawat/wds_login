70d235b10e46a927ec75dfee98e9589b433b2bcc
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-09-05 10:53:37 +0530

    authenication is checked based on user can access the home

--- server.js
+++ server.js
@@ -29,25 +29,25 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.get('/', (req, res) =>{
-  res.render('index.ejs', {name: 'Ravindra'})
+app.get('/',checkAuthenticated, (req, res) =>{
+  res.render('index.ejs', {name: req.user.name})
 })
 
-app.get('/login', (req, res) =>{
+app.get('/login', checkNotAuthenticated, (req, res) =>{
   res.render('login.ejs')
 })
 
-app.post('/login', passport.authenticate('local', {
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login', 
   failureFlash: true
 }))
 
-app.get('/register', (req, res) =>{
+app.get('/register', checkNotAuthenticated, (req, res) =>{
   res.render('register.ejs')
 })
 
-app.post('/register', async(req, res) =>{
+app.post('/register', checkNotAuthenticated,async(req, res) =>{
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
 
@@ -65,5 +65,24 @@ app.post('/register', async(req, res) =>{
   console.log(users)
 })
 
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
 
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/') 
+  }
+
+  next()
+}
 app.listen(5000)
